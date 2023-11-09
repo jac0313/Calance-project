775818f4975d3486ae391058fba387123daa2bb4
jac0313 <amelia.castaneda@ymail.com>
2023-11-09 15:54:48 -0700

    updated index.js with comments

--- index.js
+++ index.js
@@ -22,42 +22,56 @@ $.get(apiUrl).then((data) =>
   })
 )
 function tableToCSV() {
-    var csvData = [];
+ 
     // Variable to store the final csv data
-    var rows = document.getElementsByTagName('tr')
-    for(var i=0; i<rows.length; i++)
-    // Gets data from each row
-    var cols = rows[i].querySelectorAll('th,td')
-        // Gets data from each column
-        var csvRow = [];
-        // Stores each csv row data
-        for (var j = 0; j < cols.length; j++) {
-            
-            csvRow.push(cols[j].innerHTML);
-        }
-        // Gets the text data of each cell of a row and pushes it to csvRow
-        csvData.push(csvRow.join(","));
-    }
-     // Combines each column value with comma
-    csvData = csvData.join('\n');
-    // Combines each row data with new line character
-    downloadCSVFile(csvData);
-// Call this function to download csv file 
-function downloadCSVFile(csvData) {
+    var csv_data = [];
 
-    // Create CSV file object and feed our csvData into it
-    CSVFile = new Blob([csvData], {
+    // Get data from each row
+    var rows = document.getElementsByTagName('tr');
+    for (var i = 0; i < rows.length; i++) {
+
+        // Get data from each column
+        var cols = rows[i].querySelectorAll('td,th');
+
+        // Stores each csv row data
+        var csvrow = [];
+        for (var j = 0; j < cols.length; j++) {
+
+            // Get the text data of each cell of a row and push it to csvrow
+            csvrow.push(cols[j].innerHTML);
+        }
+
+        // Combine each column value with comma
+        csv_data.push(csvrow.join(","));
+    }
+
+    // Combine each row data with new line character
+    csv_data = csv_data.join('\n');
+
+    // Call this function to download csv file  
+    downloadCSVFile(csv_data);
+
+}
+
+function downloadCSVFile(csv_data) {
+
+    // Create CSV file object and feed our csv_data into it
+    CSVFile = new Blob([csv_data], {
         type: "text/csv"
     });
+
     // Create to temporary link to initiate download process
     var temp_link = document.createElement('a');
+
     // Download csv file
     temp_link.download = "GfG.csv";
     var url = window.URL.createObjectURL(CSVFile);
     temp_link.href = url;
-    // This link should not be displayed, solely for triggering purposes
+
+    // This link should not be displayed
     temp_link.style.display = "none";
     document.body.appendChild(temp_link);
+
     // Automatically click the link to trigger download
     temp_link.click();
     document.body.removeChild(temp_link);
